2383709a48d81c175c6b592f492cbf60f0066e18
membrain12 <39191142+membrain12@users.noreply.github.com>
2023-11-24 13:33:10 -0600

    Add operate function to call correct math function

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function multiply(n1, n2) {
 function divide(n1, n2) {
     return n1 / n2;
 }
+
+function operate(op, n1, n2) {
+    if (op == "+") {
+        return add(n1, n2);
+    } else if (op == "-") {
+        return subtract(n1, n2);
+    } else if (op == "*") {
+        return multiply(n1, n2);
+    } else if (op == "/") {
+        return divide(n1, n2);
+    } else {
+        return;
+    }
+}
+
+let firstNum;
+let operation;
+let secNum;
